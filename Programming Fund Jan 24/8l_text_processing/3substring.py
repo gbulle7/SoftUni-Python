b5b17104e033ce36f272688d860d031e92a82fda
gbulle7 <144064831+gbulle7@users.noreply.github.com>
2024-03-26 02:30:38 +0200

    Text Processing Lab

--- Programming Fund Jan 24/8l_text_processing/3substring.py
+++ Programming Fund Jan 24/8l_text_processing/3substring.py
@@ -4,3 +4,15 @@ second_string = input()
 while first_string in second_string:
     second_string = second_string.replace(first_string, '')
 print(second_string)
+
+# def replace_substr(string: str, substring: str):
+#     if substring not in string:
+#         return string
+#
+#     result = string.replace(substring, '')
+#     return replace_substr(result, substring)
+#
+#
+# remove_string = input()
+# text = input()
+# print(replace_substr(text, remove_string))

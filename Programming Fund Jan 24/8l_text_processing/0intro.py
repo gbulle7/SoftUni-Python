9793080e593db3ebde7cfcd187fb483afdff8173
gbulle7 <144064831+gbulle7@users.noreply.github.com>
2024-03-26 02:18:00 +0200

    Text Processing Lab

--- Programming Fund Jan 24/8l_text_processing/0intro.py
+++ Programming Fund Jan 24/8l_text_processing/0intro.py
@@ -1,14 +1,39 @@
 a = 'aSd'
 b = a.swapcase()
-print(b)            # AsD
+print(b)                         # AsD
 c = a.capitalize()
-print(c)            # Asd
+print(c)                         # Asd
 d = a.upper()
-print(d)            # ASD
+print(d)                         # ASD
 e = a.lower()
-print(e)            # asd
+print(e)                         # asd
 f = a.find('S')
-print(f)            # 1
+print(f)                         # 1
 
-txt = "I like bananas"
-print(txt.replace("bananas", "apples"))      # I like apples
+txt1 = "I like bananas"
+print(txt1.replace("bananas", "apples"))      # I like apples
+
+print(id(txt1))      # Object identity/memory address
+
+
+# center string with padding
+txt2 = "banana"
+x = txt2.center(20, "O")
+print(x)                           # OOOOOOObananaOOOOOOO
+x = txt2.rjust(20, 'O')     # OOOOOOOOOOOOOObanana
+print(x)
+x = txt2.ljust(20, 'O')     # bananaOOOOOOOOOOOOOO
+print(x)
+
+# mapping table for translation
+txt3 = "Good night Sam!"
+x1 = "mSa"
+y1 = "eJo"
+z1 = "odnght"
+mytable = str.maketrans(x1, y1, z1)
+print(txt3.translate(mytable))      # G i Joe!
+
+# same as dictionary
+txt4 = "Good night Sam!"
+mydict = {109: 101, 83: 74, 97: 111, 111: None, 100: None, 110: None, 103: None, 104: None, 116: None}
+print(txt4.translate(mydict))        # G i Joe!
--- Programming Fund Jan 24/8l_text_processing/2repeat_strings.py
+++ Programming Fund Jan 24/8l_text_processing/2repeat_strings.py
@@ -5,3 +5,7 @@ for word in sequence:
     result += word * len(word)
 
 print(result)
+
+# data = input().split()
+# repeat_text = [text * len(text) for text in data]
+# print(''.join(repeat_text))

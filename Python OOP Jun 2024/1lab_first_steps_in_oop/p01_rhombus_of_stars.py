b24693818b576ed27dc4a3675ad67589af345aa3
gbulle7 <144064831+gbulle7@users.noreply.github.com>
2024-07-02 15:21:33 +0300

    Python OOP First Steps Lab

---
+++ Python OOP Jun 2024/1lab_first_steps_in_oop/p01_rhombus_of_stars.py
@@ -0,0 +1,21 @@
+def print_rhombus(size):
+    for star_count in range(1, size):
+        print_row(size, star_count)
+    for star_count in range(size, 0, -1):
+        print_row(size, star_count)
+
+
+# def print_row(size, star_count):
+#     for row in range(size - star_count):
+#         print(" ", end="")
+#     for row in range(1, star_count):
+#         print("*", end=" ")
+#     print("*")
+
+
+def print_row(size, row):
+    print(f"{" " * (size - row)}{"* " * row}")
+
+
+size = int(input())
+print_rhombus(size)
